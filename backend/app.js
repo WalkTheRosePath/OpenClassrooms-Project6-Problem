b2d45088d0dea4e768c79c7d82dc4a92cc17eecc
Jessica Williams <walktherosepath@gmail.com>
2024-03-31 13:19:39 -0600

    first commit

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+// MONGODB PW: Mothermary3@
+// MONGODB CONNECTION: mongodb+srv://jessica:<password>@cluster0.vnvkk29.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
