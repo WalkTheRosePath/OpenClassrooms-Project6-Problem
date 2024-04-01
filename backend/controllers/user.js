b4bc0662d49e6576374b228ff9bbe9a242520577
Jessica Williams <walktherosepath@gmail.com>
2024-04-01 11:04:14 -0600

    created comments for user.js file in controllers

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -34,30 +34,37 @@ exports.signup = (req, res, next) => {
 // Define a controller function for handling user login
 // This function will receive the request object, attempt to find the user by email in the database, compare the password, and generate a JWT token if authentication succeeds
 exports.login = (req, res, next) => {
+    // Find user by email in the database
     User.findOne({ email: req.body.email }).then(
         (user) => {
+            // If user is not found, return an error response
             if (!user) {
                 return res.status(401).json({
                     error: new Error('User not found!')
                 });
             }
+            // Compare passwords using bcrypt
             bcrypt.compare(req.body.password, user.password).then(
                 (valid) => {
+                    // If password is incorrect, return an error response
                     if (!valid) {
                         return res.status(401).json({
                             error: new Error('Incorrect password!')
                         });
                     }
+                    // Generate JWT token
                     const token = jwt.sign(
                         { userId: user._id },
                         'RANDOM_TOKEN_SECRET',
                         { expiresIn: '24h' });
+                    // Send successful login response with token
                     res.status(200).json({
                         userId: user._id,
                         token: token
                     });
                 }
             ).catch(
+                // Handle bcrypt error
                 (error) => {
                     res.status(500).json({
                         error: error
@@ -66,6 +73,7 @@ exports.login = (req, res, next) => {
             );
         }
     ).catch(
+        // Handle database error
         (error) => {
             res.status(500).json({
                 error: error
