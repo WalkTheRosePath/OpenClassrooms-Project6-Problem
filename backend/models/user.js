305c9cdbc9045227af68565d095251c57c2ace92
Jessica Williams <walktherosepath@gmail.com>
2024-04-01 11:07:54 -0600

    created comments for user.js in models

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+// Define the schema for the User model
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
+// Use plugin to enforce uniqueness of email field
 userSchema.plugin(uniqueValidator);
 
+// Export the User model with the defined schema
 module.exports = mongoose.model('User', userSchema);
