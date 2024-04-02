89d44868ef9f04e8b356b70b247440e4a392db0e
Jessica Williams <walktherosepath@gmail.com>
2024-04-02 15:53:23 -0600

    fixed comments and renamed variables for sauce.js in routes

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -8,18 +8,18 @@ const express = require('express');
 const router = express.Router();
 
 // Import middleware functions for authentication and file upload
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+const authMiddleware = require('../middleware/auth');
+const multerMiddleware = require('../middleware/multer-config');
 
 // Import controller functions for 'sauces' endpoints
 const saucesController = require('../controllers/sauces');
 
 // Define routes for 'sauces' endpoints
-router.get('/', auth, saucesController.getAllSauces); // GET request to fetch all 'sauces' items
-router.get('/:id', auth, saucesController.getOneSauce); // GET request to fetch a single 'sauce' item by ID
-router.post('/', auth, multer, saucesController.createSauce); // POST request to create a new 'sauce' item
-router.put('/:id', auth, multer, saucesController.modifySauce); // PUT request to update a 'sauce' item by ID
-router.delete('/:id', auth, saucesController.deleteSauce); // DELETE request to delete a 'sauce' item by ID
-// router.post('/:id/like', auth, saucesController.likeOrDislikeSauce); // POST request to like or dislike a 'sauce'
+router.get('/', authMiddleware, saucesController.getAllSauces); // GET request to fetch all 'sauces' items
+router.get('/:id', authMiddleware, saucesController.getOneSauce); // GET request to fetch a single 'sauce' item by ID
+router.post('/', authMiddleware, multerMiddleware, saucesController.createSauce); // POST request to create a new 'sauce' item
+router.put('/:id', authMiddleware, multerMiddleware, saucesController.modifySauce); // PUT request to update a 'sauce' item by ID
+router.delete('/:id', authMiddleware, saucesController.deleteSauce); // DELETE request to delete a 'sauce' item by ID
+router.post('/:id/like', authMiddleware, saucesController.likeOrDislikeSauce); // POST request to like or dislike a 'sauce'
 
 module.exports = router;
