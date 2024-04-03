4452e750e0b549f2dc7f787e89d40c44e153ee55
Jessica Williams <walktherosepath@gmail.com>
2024-04-03 09:36:53 -0600

    organized comments in sauce.js in routes

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -14,13 +14,16 @@ const multer = require('../middleware/multer-config');
 // Import controller functions for 'sauces' endpoints
 const saucesController = require('../controllers/sauces');
 
-// Define routes for 'sauces' endpoints
-//TODO Check routes on specs
+// Define routes for 'sauces' endpoints  
 router.get('/', auth, saucesController.getAllSauces); // GET request to fetch all 'sauces' items
 router.get('/:id', auth, saucesController.getOneSauce); // GET request to fetch a single 'sauce' item by ID
+
 router.post('/', auth, multer, saucesController.createSauce); // POST request to create a new 'sauce' item
 router.put('/:id', auth, multer, saucesController.modifySauce); // PUT request to update a 'sauce' item by ID
+
 router.delete('/:id', auth, saucesController.deleteSauce); // DELETE request to delete a 'sauce' item by ID
+
 router.post('/:id/like', auth, saucesController.likeOrDislikeSauce); // POST request to like or dislike a 'sauce'
 
+// Export routes
 module.exports = router;
